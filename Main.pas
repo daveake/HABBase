76246af87b4236180caaf13c899438b74f6afe0c
Dave Akerman <dave@sccs.co.uk>
2020-03-04 15:58:55 +0000

    Main form

---
+++ Main.pas
@@ -0,0 +1,37 @@
+unit Main;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, AdvSplitter;
+
+type
+  TForm1 = class(TForm)
+    AdvSplitter1: TAdvSplitter;
+    Panel1: TPanel;
+    AdvSplitter2: TAdvSplitter;
+    Panel3: TPanel;
+    AdvSplitter3: TAdvSplitter;
+    Panel4: TPanel;
+    Panel5: TPanel;
+    Panel2: TPanel;
+    AdvSplitter4: TAdvSplitter;
+    AdvSplitter5: TAdvSplitter;
+    Panel6: TPanel;
+    Panel7: TPanel;
+    Panel8: TPanel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+end.
